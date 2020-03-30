27e7abf5930633140e9328b1095f7d53f94e5735
Ivan Tsimbalist <iv-tsim@yandex.ru>
2020-03-30 14:23:10 +0300

    bug fixes

--- lesson03/script.js
+++ lesson03/script.js
@@ -7,12 +7,12 @@ const lang = "en",
 if (lang === "ru"){
     console.log(weekday[0][0]);
 } else if(lang === "en"){
-    console.log(weekday[0][0]);
+    console.log(weekday[1][0]);
 }
 
 switch(lang){
     case "en":
-        console.log(weekday[0][0]);
+        console.log(weekday[1][0]);
         break;
     case "ru":
         console.log(weekday[0][0]);
