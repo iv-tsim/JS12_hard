34e256123a97ea1d6fda2697e3b2796db1e0618e
Ivan Tsimbalist <iv-tsim@yandex.ru>
2020-03-31 10:52:46 +0300

    correct 1.c task

--- lesson03/script.js
+++ lesson03/script.js
@@ -1,26 +1,26 @@
 const lang = "en",
-    weekday = [
-    ["Понедельник, вторник, среда, четверг, пятница"],
-    ["Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday"]
-];
+    weekday = new Map([
+        ["ru", "Понедельник, вторник, среда, четверг, пятница"],
+        ["en", "Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday"]
+    ]);
 
 if (lang === "ru"){
-    console.log(weekday[0][0]);
+    console.log(weekday.get(lang));
 } else if(lang === "en"){
-    console.log(weekday[1][0]);
+    console.log(weekday.get(lang));
 }
 
 switch(lang){
     case "en":
-        console.log(weekday[1][0]);
+        console.log(weekday.get(lang));
         break;
     case "ru":
-        console.log(weekday[0][0]);
+        console.log(weekday.get(lang));
         break;
     default: console.log("Something went wrong");
 }
 
-(lang === "ru") ? console.log(weekday[0][0]) : console.log(weekday[1][0]);
+console.log(weekday.get(lang));
 
 const namePerson = "Артем";
 
