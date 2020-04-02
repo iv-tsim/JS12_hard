db80fb8f27a5f925c7e024eb006a6c77e25f8acb
Ivan Tsimbalist <iv-tsim@yandex.ru>
2020-04-02 10:17:26 +0300

    change method in 1 task

--- lesson05/script.js
+++ lesson05/script.js
@@ -1,10 +1,10 @@
 const arr = [1234, 234, 7654, 4567, 758, 4758, 7748];
 
-for (let i = 0; i< arr.length; i++) {
-    if (arr[i].toString().slice(0, 1) === "2" || arr[i].toString().slice(0, 1) === "4") {
-        console.log(arr[i]);
+arr.filter(function(number){
+    if (number.toString().slice(0, 1) === "2" || number.toString().slice(0, 1) === "4") {
+        console.log(number);
     }
-}
+});
 
 const n = 100;
 
